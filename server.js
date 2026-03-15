bc0cfe4964629fab0f3f9f3e8b946f1aa3de7754
lukerosetti <ssc28.48@gmail.com>
2026-03-15 14:41:50 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html for all routes (SPA)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`
+  🏀 March Madness Bracket Challenge is running!
+  
+  Open your browser to: http://localhost:${PORT}
+  
+  Press Ctrl+C to stop the server.
+  `);
+});
